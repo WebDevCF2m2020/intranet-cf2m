8c40c977d9fb84036ff78be3adc3c3c917794d12
ReinWired <palmisanoandre@hotmail.com>
2021-03-25 14:45:06 +0100

    Fixed ManagerTableInterface

--- model/ManagerTableInterface.php
+++ model/ManagerTableInterface.php
@@ -7,8 +7,8 @@ interface ManagerTableInterface {
 	public function selectAllNonActif():array;	
     public function selectAll():array;
 	
-	public function selectAllActif():array;
-	public function selectAllNonActif():array;	
+	public function selectOneActif():array;
+	public function selectOneNonActif():array;	
 	public function selectOne():array;
 	
 	// La valeur de retour pour les méthodes qui ne sont pas des SELECT sera un array contenant le résultat de l'execute (booléen qui indique si la requête a été un succès) et en deuxième position un message d'erreur en cas d'échec
