4c8fdff0cfcf5f15a3be77d974f8e200ac321d53
CHIHAB09 <web2020.chihab@gmail.com>
2021-03-26 09:12:38 +0100

    Create request

--- model/DroitManager.php
+++ model/DroitManager.php
@@ -2,6 +2,10 @@
 
 class DroitManager extends ManagerTableAbstract implements ManagerTableInterface
 {
-    
-    
+    //put your query for selected all datas in this table
+    public function selectAll(): array {
+        $sql = "SELECT * FROM ledroit;";
+        $query = $this->db->query($sql);
+        
+    }
 }
