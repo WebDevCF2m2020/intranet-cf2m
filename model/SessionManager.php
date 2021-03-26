d600586084115bf0fd4b3e626ebb2158e0fe44f8
CHIHAB09 <web2020.chihab@gmail.com>
2021-03-26 09:02:10 +0100

    Create request

--- model/SessionManager.php
+++ model/SessionManager.php
@@ -2,5 +2,10 @@
 
 class SessionManager extends ManagerTableAbstract implements ManagerTableInterface
 {
-    
+    //put your query for selected all datas in this table
+    public function selectAll(): array {
+        $sql = "SELECT * FROM lasession;";
+        $query = $this->db->query($sql);
+        
+    }
 }
